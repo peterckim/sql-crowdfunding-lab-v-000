d70b86c46b925ff97a67422b3bda80d3714a7729
Peter Kim <peterckim96@gmail.com>
2018-06-29 00:27:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, COUNT(pledges.amount)
+"SELECT projects.category, pledges.amount
 FROM projects LEFT JOIN pledges ON projects.id = pledges.project_id
 WHERE projects.category = 'music'"
 end
